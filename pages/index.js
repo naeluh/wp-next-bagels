07a329d1d42b211d5e720578554e3cbf3640bda2
naeluh <naeluh@gmail.com>
2020-10-24 15:49:12 -0700

    try to get a build to work

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,8 @@ import BagelDefinition from '../components/bagelDefinition';
 import { getAllPostsForHome, getHomePageData } from '../lib/api';
 import { CMS_NAME } from '../lib/constants';
 
-export default function Index({ allPosts: { edges }, preview, homeData }) {
+export default function Index({ preview, homeData }) {
   console.log(homeData);
-  const heroPost = edges[0]?.node;
-  const morePosts = edges.slice(1);
-
   return (
     <Layout preview={preview}>
       <Head>
@@ -33,15 +30,6 @@ export default function Index({ allPosts: { edges }, preview, homeData }) {
           quote={homeData.bagelDefinition.bagelQuote}
           desc={homeData.bagelDefinition.bagelDescription}
         />
-        {heroPost && (
-          <HeroPost
-            title={heroPost.title}
-            date={heroPost.date}
-            slug={heroPost.slug}
-            excerpt={heroPost.excerpt}
-          />
-        )}
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
       </Container>
     </Layout>
   );
@@ -50,8 +38,7 @@ export default function Index({ allPosts: { edges }, preview, homeData }) {
 export async function getStaticProps({ preview = false }) {
   const homeData = await getHomePageData();
   const allPosts = await getAllPostsForHome(preview);
-  // console.log(homeData);
   return {
-    props: { allPosts, preview, homeData },
+    props: { preview, homeData },
   };
 }
