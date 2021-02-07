99fe0cb21bcec45246a793d332062ae5265d3b79
naeluh <naeluh@gmail.com>
2021-02-07 11:54:14 -0800

    Update site

--- components/bagelSetAddRemove.js
+++ components/bagelSetAddRemove.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
-import Modal from './modal';
-import useRouter from 'next/router';
+import updateAction from '../lib/updateAction';
+import { useStateMachine } from 'little-state-machine';
+
+const Button = ({ id }) => {
+  <button></button>;
+};
 
 const bagelSetAddRemove = ({ bagelSelection }) => {
+  const { state, action } = useStateMachine(updateAction);
+  const removeBagelSet = (array, value) => {
+    action({
+      bagelSelections: array.filter(item => {
+        return item.id !== value;
+      }),
+    });
+    console.log(state);
+  };
   return (
     <div className='my-4'>
       <h4>
@@ -23,13 +36,14 @@ const bagelSetAddRemove = ({ bagelSelection }) => {
         </Link>
       </p>
       <p>
-        <Modal />
-        <Link
-          href={`/bagels/remove-bagels?bagelSelectionsID=${bagelSelection.id}&type=${bagelSelection.bagelSetType}`}
-          as={`/bagels/remove-bagels`}
+        <button
+          onClick={() =>
+            removeBagelSet(state.data.bagelSelections, bagelSelection.id)
+          }
+          type='button'
         >
           Remove
-        </Link>
+        </button>
       </p>
     </div>
   );
