876665a1781c98701f2fb5173c006076998498b1
naeluh <naeluh@gmail.com>
2021-02-13 16:59:14 -0800

    remove secrets

--- next.config.js
+++ next.config.js
@@ -1,13 +1,1 @@
-module.exports = {
-  env: {
-    MY_SECRET_KEY: process.env.MY_SECRET_KEY,
-    WORDPRESS_API_URL: 'https://mamalagels.com/graphql',
-    WORDPRESS_AUTH_REFRESH_TOKEN:
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbWFtYWxhZ2Vscy5jb20iLCJpYXQiOjE1OTg3MjYyMDIsIm5iZiI6MTU5ODcyNjIwMiwiZXhwIjoxNTk4NzI2NTAyLCJkYXRhIjp7InVzZXIiOnsiaWQiOiIxIn19fQ.ZP3FcdC8olFyvVJvBylVCqjmSfHuxmWwnMW8BjzeS-U',
-    WORDPRESS_PREVIEW_SECRET:
-      '9flAO>PMa%?:78A5$|cbm=Lu#9$@*#rWQZ^`Fh6}nd!}dzgu(UWn{Og,?X+;%IuZ',
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: 'pk_12345',
-    STRIPE_SECRET_KEY: 'sk_12345',
-    STRIPE_WEBHOOK_SECRET: 'whsec_1234',
-  },
-};
+module.exports = {};
