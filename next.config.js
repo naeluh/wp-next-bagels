9d0bf43377a17007881ba659670b19d56fa60318
naeluh <naeluh@gmail.com>
2021-02-15 09:23:51 -0800

    Update enviroment

--- next.config.js
+++ next.config.js
@@ -1,1 +1,22 @@
-module.exports = {};
+// next.config.js
+const Dotenv = require('dotenv-webpack');
+console.log(process.env);
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add the new plugin to the existing webpack plugins
+    config.plugins.push(new Dotenv({ silent: true }));
+
+    return config;
+  },
+  env: {
+    WORDPRESS_API_URL: process.env.WORDPRESS_API_URL,
+    WORDPRESS_AUTH_REFRESH_TOKEN: process.env.WORDPRESS_AUTH_REFRESH_TOKEN,
+    WORDPRESS_PREVIEW_SECRET: process.env.WORDPRESS_PREVIEW_SECRET,
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+    USER: process.env.USER,
+    PASS: process.env.PASS,
+  },
+};
