1cb67064696d4b20ec6f21a90573669c9a54ad43
naeluh <naeluh@gmail.com>
2021-04-04 14:49:21 -0700

    Update image

--- components/FeaturedBagel.js
+++ components/FeaturedBagel.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './featuredBagel.module.css';
+import Img from 'next/image';
 
 const FeaturedBagel = ({ title, subtitle, img }) => {
   return (
     <section className='px-5 py-8 flex-col md:flex-row flex lg:items-center md:justify-between mb-16 md:mb-24 md:px-8 md:py-8 md:pb-12 border-m-yellow border-12'>
       <div className={`mb-6 md:mb-0 flex-1 md:mr-8 lg:mr-16`}>
-        <img
+        <Img
           className={`object-cover h-full w-full relative`}
           src='/static/images/penguin-city-pretzel.jpg'
+          alt='bagel'
+          width={1440}
+          height={1440}
         />
       </div>
       <div className={`flex-1 relative`}>
