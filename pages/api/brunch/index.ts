90d44ba36dc02f1b73b4428ccf9e8ddf8c93f952
naeluh <naeluh@gmail.com>
2021-05-04 20:38:48 -0700

    add api

---
+++ pages/api/brunch/index.ts
@@ -0,0 +1,46 @@
+import fetch from '../../../lib/fetch';
+import btoa from 'btoa';
+
+export default async function wordPressPost(req: any, res: any) {
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', 'GET');
+    return res.status(405).end();
+  }
+
+  if (!process.env.PASS && process.env.USER) {
+    return res.status(401).json({
+      errors: [{ message: 'MISSING PASS OR USER' }],
+    });
+  }
+
+  const SMALL = req.query.small;
+  const LARGE = req.query.large;
+
+  const response = await fetch(
+    `https://mamalagels.com/wp-json/acf/v3/brunch_bag/cG9zdDo2ODQy?fields[small]=${SMALL}&fields[large]=${LARGE}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/plain',
+        Authorization:
+          'Basic ' + btoa(process.env.USER + ':' + process.env.PASS),
+      },
+    }
+  );
+
+  if (response.ok) {
+    // Cache the Wordpress response for 3 seconds
+    res.setHeader('Cache-Control', 's-maxage=3, stale-while-revalidate');
+    res.status(200).json({
+      success: [{ message: `${response.status}` }],
+    });
+  } else {
+    res.status(400).json({
+      errors: [
+        {
+          message: `Fetch to the Wordpress API failed with code: ${response.status}`,
+        },
+      ],
+    });
+  }
+}
